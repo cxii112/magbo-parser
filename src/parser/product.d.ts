eeee0bc8e71e50d5ece034fb4f45011733c3f92b
cxii112 <lyakhovoy.d@ya.ru>
2025-12-10 18:00:21 +0500

    парсер страницы товара

---
+++ src/parser/product.d.ts
@@ -0,0 +1,18 @@
+export type Product = Expand<
+  {
+    title: string
+    inStock: boolean
+  } & Prices &
+    Properties
+>
+
+export type Prices = {
+  price: number | undefined
+  discountPrice: number | undefined
+}
+
+export type Properties = {
+  art: string | undefined
+  producer: string | undefined
+  id: string | undefined
+}
