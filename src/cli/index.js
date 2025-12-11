c0863c0b26d8e2cfde78a197080a911ebd7fe600
cxii112 <lyakhovoy.d@ya.ru>
2025-12-11 15:56:31 +0500

    скрипт скрепера

--- src/cli/index.js
+++ src/cli/index.js
@@ -1,1 +1,39 @@
-console.log("Будующая точка входа")
+import { csv } from "../export/csv.js"
+import { page } from "../scrapper/page.js"
+import fs from "node:fs"
+import { makeSearchURL } from "../scrapper/url.js"
+
+const baseUrl = process.env.BASE_URL || "https://magbo.ru"
+const maxRetries = parseInt(process.env.NAX_RETRIES) || 0
+const args = process.argv.slice(2)
+const filepath = args[0]
+if (!filepath.endsWith(".csv")) {
+  throw new Error(`${filepath} должен быть .csv файлом`)
+}
+let stream = undefined
+try {
+  stream = fs.createWriteStream(filepath)
+} catch (err) {
+  throw err
+}
+
+const requests = args.slice(1).map((arg) => {
+  return makeSearchURL(arg, baseUrl)
+})
+
+let products = []
+
+await Promise.all(
+  requests.map((req) => {
+    return page(req, { baseUrl: baseUrl, maxRetries: maxRetries })
+      .then((prods) => {
+        products.push(...prods)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }),
+)
+csv(products, stream, {
+  sep: ";",
+})
